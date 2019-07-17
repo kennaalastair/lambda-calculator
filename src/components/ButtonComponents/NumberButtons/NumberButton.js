db6cb3765f23e44aa47bff0e6b5cec436114b46a
Kenna <kennaalastair@gmail.com>
2019-07-17 07:55:49 -0700

    display not working yet

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = ({ number }) => {
   return (
-    <>
+    <button className="number-btn">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {number}
+    </button>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
-
+import { specials } from "../../../data.js";
 //Import your array data to from the provided data file
+import SpecialButton from "./SpecialButton.js";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(specials);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specials.map((misc) => <SpecialButton special={misc} />)}
     </div>
   );
 };
+
+export default Specials;
